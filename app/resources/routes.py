84bad57b51c265f78ebef63f0ea687d14a4a59a4
Jesse Bartola <jrbartola@gmail.com>
2018-02-28 17:29:15 -0500

    Flask backend

---
+++ app/resources/routes.py
@@ -0,0 +1,22 @@
+'''
+routes.py
+
+This file hosts the routes that will be used by the flask object
+
+@authors: CS320
+'''
+
+from flask import render_template
+from flask_restful import Api
+
+from app import app
+from resources.api import User
+
+api = Api(app)
+
+# Index page
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+api.add_resource(User, '/api/user/<string:userid>', '/api/user')
